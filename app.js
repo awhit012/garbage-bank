c5ab1c7cdeb0ed65541ef4498d12d3e99ee74405
Alex White <awhit012@gmail.com>
2017-04-29 15:40:04 -0700

    moved from another repo

---
+++ app.js
@@ -0,0 +1,63 @@
+function Bank() {
+	this.baseUrl = 'https://garbagecitybank.firebaseio.com';
+	this.storiesExt = '/stories.json';
+	this.storiesList = $('#stories-list');
+	this.form = $('#form');
+	this.storyTextArea = $("#textarea");
+	this.horseIcon = '<img src="./assets/horse.png" style="width:60px;">'
+
+}
+
+Bank.prototype.getStories = function() {
+	$.get(this.baseUrl + this.storiesExt, function(res, err) {
+		console.log(res);
+		app.displayStories(res);
+	})
+}
+
+Bank.prototype.postStory = function(data) {
+	$.post(this.baseUrl + this.storiesExt, JSON.stringify(data), function(res, status) {
+		console.log(res)
+		console.log(status)
+  }).done(function() {
+		app.storiesList.prepend("<li class='list-group-item'>" + app.horseIcon + data + "</li>")
+  })
+}
+
+Bank.prototype.displayStories = function(stories) {
+	var app = this;
+	var tempArray = [];
+	$.each(stories, function(index) {
+		storyText = stories[index];
+		tempArray.push(storyText);
+	});
+	$.each(tempArray.reverse(), function(index) {
+		app.storiesList.append("<li class='list-group-item'>" + app.horseIcon + tempArray[index] + "</li>")
+	})
+}
+
+Bank.prototype.addEventListeners = function() {
+	var app = this;
+	this.form.on('submit', function(event) {
+		event.preventDefault();
+		console.log(app.storyTextArea.val());
+		app.postStory(app.storyTextArea.val().trim());
+		app.storyTextArea.val("")
+	})
+}
+
+
+app = new Bank();
+app.getStories();
+app.addEventListeners();
+
+
+$( document ).ready(function() {
+	var offset = $(".wagons").offset();
+	$(window).scroll(function(event) {
+	  var st = $(this).scrollTop();
+	  $(".wagons").css("left", st + offset.left);
+	});
+});
+
+
